1982a4342ca4e8c7d39f15ad0044400aa7059629
Malaparte4 <113225642+Malaparte4@users.noreply.github.com>
2024-05-17 21:09:12 -0500

    Chromosome standard methods

--- src/chromosome.rs
+++ src/chromosome.rs
@@ -6,7 +6,19 @@ pub struct Chromosome {
 }
 
 impl Chromosome {
-    fn new(genes: Vec<Gene>) -> Self {
+    fn new() -> Self {
+        Chromosome { genes: Vec::new() }
+    }
+
+    fn new_from_vec(genes: Vec<Gene>) -> Self {
         Chromosome { genes }
+    }
+
+    fn push(&mut self, gene: Gene) {
+        self.genes.push(gene);
+    }
+
+    fn append_vec(&mut self, genes: Vec<Gene>) {
+        todo!();
     }
 }
