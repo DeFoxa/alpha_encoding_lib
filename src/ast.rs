3702c8bd5298b4e51bdf2ef8e9671dc75f2b01e9
DeFoxa <113225642+DeFoxa@users.noreply.github.com>
2024-08-13 12:59:10 -0500

    move ast dev to ast branch, remove unused chromosome

--- src/ast.rs
+++
@@ -1,6 +0,0 @@
-use crate::{
-    data::types::*,
-    features::*,
-    gene::{FunctionData, FunctionNode, GeneType, TerminalData},
-};
-use sokoban::ast::AST;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 #![allow(warnings)]
-pub mod ast;
-pub mod chromosome;
 pub mod data;
 pub mod features;
 pub mod gene;
